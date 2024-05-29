f6ec2be1ba9eb5cfb625cdd6fa55030a07832aff
asadullah48 <asadullahshafique@hotmail.com>
2024-05-29 10:03:24 +0500

    Initial commit

---
+++ ans114.ts
@@ -0,0 +1,12 @@
+//Q 114
+// Creates a Map to store student IDs (keys) and names (values)
+const students = new Map<number, string>();
+students.set(1, "Asad");
+students.set(2, "Babar");
+students.set(3, "Chandu");
+
+// Iterates over the Map and logs each student ID and name
+students.forEach((name, id) => {
+  console.log(`Student ID: ${id}, Name: ${name}`);
+});
+// This loop goes through each student and logs their ID and name.
